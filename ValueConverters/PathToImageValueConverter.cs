308a02e7ecedafce2371211998fe82150ed7df2f
iwubcode <iwubcode@users.noreply.github.com>
2020-03-03 00:19:00 -0600

    By default, images are locked by WPF when they are loaded by the UI.  In order to allow users to modify textures while the tool is open, we want to prevent this locking.  The caveat is that if an image is deleted or renamed during the tool process, the resulting json outputted will not be correct.

---
+++ ValueConverters/PathToImageValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace DolphinDynamicInputTextureCreator.ValueConverters
+{
+    public class PathToImageValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            // By default, WPF will cache and lock the image file
+            // In order to avoid the lock, we explicitly set the cache option
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = new Uri(path);
+            image.EndInit();
+
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
