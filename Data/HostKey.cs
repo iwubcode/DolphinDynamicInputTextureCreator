7b4269c6e99265db2c6a0b6e7349fcdd90781668
iwubcode <iwubcode@users.noreply.github.com>
2020-03-04 22:40:23 -0600

    Remove unnecessary constructor and property in HostKey

--- Data/HostKey.cs
+++ Data/HostKey.cs
@@ -7,10 +7,6 @@ namespace DolphinDynamicInputTextureCreator.Data
 {
     public class HostKey : Other.PropertyChangedBase
     {
-        public HostKey()
-        {
-            Name = "`Button A`";
-        }
         /// <summary>
         /// The name of the key on the host machine, ex: "`Shoulder L`"
         /// </summary>
@@ -44,28 +40,6 @@ namespace DolphinDynamicInputTextureCreator.Data
                 _texture_path = value;
                 OnPropertyChanged(nameof(TexturePath));
             }
-        }
-
-        private ICommand _change_image_command;
-        public ICommand ChangeImageCommand
-        {
-            get
-            {
-                if (_change_image_command == null)
-                {
-                    _change_image_command = new RelayCommand(ChangeImage);
-                }
-                return _change_image_command;
-            }
-        }
-
-        // TODO: make this MVVM compliant
-        private void ChangeImage(object obj)
-        {
-            var dialog = new OpenFileDialog { InitialDirectory = Path.GetDirectoryName(_texture_path) };
-            dialog.ShowDialog();
-
-            TexturePath = dialog.FileName;
         }
     }
 }
