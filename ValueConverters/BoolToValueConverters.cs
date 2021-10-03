ed54adbec50c90effa7abc2f2d41601fc289cc92
Venomalia <Venomalia@mailbox.org>
2021-10-03 13:21:42 +0200

    outline a hash that is not valid with red.

---
+++ ValueConverters/BoolToValueConverters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace DolphinDynamicInputTextureCreator.ValueConverters
+{
+
+    [ValueConversion(typeof(bool), typeof(System.Windows.Media.SolidColorBrush))]
+    public class BoolToSolidColorBrushConverter : BoolToValueConverter<System.Windows.Media.SolidColorBrush> {}
+
+    public abstract class BoolToValueConverter<T> : IValueConverter
+    {
+        public T TrueValue { get; set; }
+        public T FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? FalseValue : ((bool)value ? TrueValue : FalseValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.Equals(TrueValue);
+        }
+    }
+}
